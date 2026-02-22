72bd8cfdb2c0f1578fc2c189b80b77ecfed68589
Anish Kumar Singh <singhrajputanish535@gmail.com>
2026-02-22 16:01:16 +0530

    Update MongoDB connection to use environment variable

--- src/config.js
+++ src/config.js
@@ -1,18 +1,22 @@
-const mongoose = require('mongoose');
-const connect = mongoose.connect("mongodb://localhost:27017/Login");
+const mongoose = require("mongoose");
 
-// Check database connected or not
-connect.then(() => {
+//  Connect using Render Environment Variable
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
     console.log("Database Connected Successfully");
 })
-.catch(() => {
+.catch((err) => {
     console.log("Database cannot be Connected");
-})
+    console.log(err);
+});
 
 // Create Schema
 const Loginschema = new mongoose.Schema({
     name: {
-        type:String,
+        type: String,
         required: true
     },
     password: {
@@ -21,7 +25,7 @@ const Loginschema = new mongoose.Schema({
     }
 });
 
-// collection part
-const collection = new mongoose.model("users", Loginschema);
+// Collection
+const collection = mongoose.model("users", Loginschema);
 
 module.exports = collection;
